c716c255e6ee5fa4868be40947ac0f6516ad58c6
Bryan Potter <potterbm@gmail.com>
2014-04-11 17:02:18 -0700

    gear

--- js/whitewater.js
+++ js/whitewater.js
@@ -18,6 +18,10 @@ $(document).ready(function(e) {
 		$(this).toggleClass("cross-icon");
 	});
 	
+	$(".gear-icon, .droplet-icon").click(function(e) {
+		$(this).toggleClass("hidden");
+	});
+	
 });
 
 
