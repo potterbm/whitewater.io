b06def91cef596a1ad5bbcb8a59457c46866ca56
Bryan Potter <potterbm@gmail.com>
2014-06-01 20:10:48 -0600

    loading view

--- js/whitewater.js
+++ js/whitewater.js
@@ -1,30 +1,5 @@
 
 
-function River(siteID)
-{
-	this.siteID = siteID;
-}
-
-River.prototype.siteID = -1;
-River.prototype.name = "";
-
-River.prototype.fetchWaterData = function(callback) {
-	var request = $$.ajax({
-		async : true,
-		crossDomain : true,
-		dataType : "json",
-		url : "http://waterservices.usgs.gov/nwis/iv/?format=json&parameterCd=00060,00065,00010&siteType=ES,ST&site=" + this.site,
-		success : function(response) {
-			if(typeof(callback) == "function") {
-				callback(response);
-			}
-		}
-	});
-	
-	return request;
-}
-
-
 
 
 
@@ -51,17 +26,6 @@ function isDesktop()
 }
 
 
-function showMenu(e)
-{
-	console.log('showMenu');
-	alert('showMenu');
-}
-
-
-function showSearch(e)
-{
-	alert('showSearch');
-}
 
 
 
